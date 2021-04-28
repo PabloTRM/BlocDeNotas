a5622f14abee9a86653b398e8f781b210c3272a9
pablocieep <pablo.torres.rojano@cieep.com>
2021-04-28 15:01:43 +0200

    ficheros añadidos

--- BlogDeNotas/Form1.cs
+++ BlogDeNotas/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,15 @@ namespace BlogDeNotas
     public partial class Form1 : Form
     {
         //Primero vamos a declarar y luego crear un objeto nota, y una lista de notas.
-       
+
         private Nota nota;
         private List<Nota> notas;
         private String descripcion;
         public Form1()
         {
-            
+
             notas = new List<Nota>();
+            rellenarFicheroLista();
             InitializeComponent();
         }
 
@@ -31,7 +33,31 @@ namespace BlogDeNotas
             //creamos una nota, comprobamos que ningún campo esté vació, no pedimos la fecha porque siempre trabajamos
             //con el local date, tanto para crearla como para actualizarla.
             nota = new Nota();
-            if (txtAutor.Text !="" && txtDescripcion.Text!="") {
+            if (txtAutor.Text != "" && txtDescripcion.Text != "")
+            {
+                StreamWriter fichero = null;
+                try
+                {
+
+                    //Crear fichero, si existe lo agrega al final
+                    fichero = File.AppendText("agenda.txt");
+                    //Escribe en el fichero
+                    fichero.WriteLine(txtAutor.Text + "#" + txtDescripcion.Text + "#" + DateTime.Now);
+
+                }
+                catch (Exception exception) { MessageBox.Show("Error al crear fichero"); }
+                finally
+                {
+                    try
+                    {
+                        if (fichero != null) fichero.Close();
+                    }
+                    catch (Exception error)
+                    {
+                        MessageBox.Show("error");
+                    }
+                }
+
                 nota.Autor = txtAutor.Text;
                 nota.Descripcion = txtDescripcion.Text;
                 nota.Fecha = DateTime.Now;
@@ -39,8 +65,10 @@ namespace BlogDeNotas
                 notas.Add(nota);
                 actualizarCb();
             }
-            else { MessageBox.Show("Rellena todos los campos");
-            
+            else
+            {
+                MessageBox.Show("Rellena todos los campos");
+
             }
 
             txtAutor.Clear();
@@ -52,13 +80,13 @@ namespace BlogDeNotas
             int index = cbConsultar.SelectedIndex;
             descripcion = cbConsultar.Text;
             //rellenamos el comboBox 
-  
-                    Nota notaelegida = notas[index];
-                    mostrarTxt();
-                   
-                    txtFechaConsultar.Text = notaelegida.Fecha.ToString();
-                    txtNombreConsultar.Text = notaelegida.Autor;
-                    txtNotaConsultar.Text = notaelegida.Descripcion;
+
+            Nota notaelegida = notas[index];
+            mostrarTxt();
+
+            txtFechaConsultar.Text = notaelegida.Fecha.ToString();
+            txtNombreConsultar.Text = notaelegida.Autor;
+            txtNotaConsultar.Text = notaelegida.Descripcion;
 
         }
         //como repito el código en eliminar y en modificar lo he extraido a una función y luego la llamo en cada caso.
@@ -74,14 +102,44 @@ namespace BlogDeNotas
 
         private void btnRemove_Click(object sender, EventArgs e)
         {
+            StreamWriter fichero = null;
             //Eliminar nota y modificiar nota funcionan bastante similiar, si el combobox tiene items y 
             //hay al menos un item seleccionado elimina o modifica
-            if (cbConsultar.Items.Count==0 || cbConsultar.SelectedItem==null) {
+            if (cbConsultar.Items.Count == 0 || cbConsultar.SelectedItem == null)
+            {
                 MessageBox.Show("ERROR, No hay nota seleccionada o no hay nota creada");
             }
             else
             {
-                notas.RemoveAt(cbConsultar.SelectedIndex);
+                try
+                {
+                    notas.RemoveAt(cbConsultar.SelectedIndex);
+
+
+                    fichero = File.CreateText("agenda.txt");
+                    foreach (Nota nota in notas)
+                    {
+                        fichero.WriteLine(nota.Autor + "#" + nota.Descripcion + "#" + nota.Fecha);
+
+                    }
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show("Error");
+                }
+                finally
+                {
+                    try
+                    {
+                        if (fichero != null) fichero.Close();
+                    }
+                    catch (Exception error)
+                    {
+                        MessageBox.Show("error");
+                    }
+                }
+
+
                 Ocultartxt();
             }
 
@@ -102,7 +160,7 @@ namespace BlogDeNotas
         {
             //método que usamos para cuando borramos o modificamos para volver a llenar los campos del comboBox
             cbConsultar.Items.Clear();
-           
+
             if (rbtnFecha.Checked)
             {
                 cbConsultar.Items.Clear();
@@ -118,7 +176,7 @@ namespace BlogDeNotas
                 {
                     cbConsultar.Items.Clear();
                     foreach (Nota nota in notas)
-                    {   
+                    {
                         cbConsultar.Items.Add(nota.Descripcion);
                     }
                 }
@@ -138,28 +196,93 @@ namespace BlogDeNotas
 
         private void btnModify_Click(object sender, EventArgs e)
         {
-            if (cbConsultar.Items.Count == 0 || cbConsultar.SelectedItem == null) { MessageBox.Show("ERROR, No hay nota seleccionada" +
-                " o no hay nota creada"); }
+            if (cbConsultar.Items.Count == 0 || cbConsultar.SelectedItem == null)
+            {
+                MessageBox.Show("ERROR, No hay nota seleccionada" +
+" o no hay nota creada");
+            }
 
             else
             {
-                
+
                 notas[cbConsultar.SelectedIndex].Autor = txtNombreConsultar.Text;
                 notas[cbConsultar.SelectedIndex].Descripcion = txtNotaConsultar.Text;
                 notas[cbConsultar.SelectedIndex].Fecha = DateTime.Now;
-               
-               
-                Ocultartxt();
+
+
+                StreamWriter fichero = null;
+                try
+                {
+                    fichero = File.CreateText("agenda.txt");
+                    foreach (Nota nota in notas)
+                    {
+                        fichero.WriteLine(nota.Autor + "#" + nota.Descripcion + "#" + nota.Fecha);
+
+                    }
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show("error");
+                }
+                finally
+                {
+                    try
+                    {
+                        if (fichero != null)
+
+                            fichero.Close();
+                    }
+                    catch (Exception error) { MessageBox.Show("error"); }
+
+                    Ocultartxt();
+                }
             }
         }
+
 
         private void cambiarCheckRadioBtn(object sender, EventArgs e)
         {
             actualizarCb();
             Ocultartxt();
         }
+        private void rellenarFicheroLista()
+        {
+            String linea = null;
+            StreamReader fichero = null;
+            try
+            {
+                fichero = File.OpenText("agenda.txt");
+                linea = fichero.ReadLine();
+                while (linea != null)
+                {
+                    String[] trozos = linea.Split('#');
+                    notas.Add(new Nota(DateTime.Parse(trozos[2]), trozos[0], trozos[1]));
+                    linea = fichero.ReadLine();
+                }
+            }
+            catch (Exception error)
+            {
 
-        
-    }
+                MessageBox.Show("error");
+            }
+            finally
+            {
+                try
+                {
+                    if (fichero != null)
+
+                        fichero.Close();
+                    
+
+                }
+                catch (Exception error) { MessageBox.Show("error"); }
+
+                
+            }
+
+        }
     
+    }
+
+
 }
